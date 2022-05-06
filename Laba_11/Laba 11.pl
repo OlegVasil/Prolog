685198afe7f2f56503392790e6df6c59e4a5a4fe
OlegVasil <100850508+OlegVasil@users.noreply.github.com>
2022-05-06 23:33:03 +0300

    Теперь точно работает

--- Laba_11/Laba 11.pl
+++ Laba_11/Laba 11.pl
@@ -91,7 +91,7 @@ maxDigitD(X,Y,M):-M1 is X mod 10, X1 is X div 10,M1 > Y,!,maxDigitD(X1,M1,M); X2
 %____17____%
 %Вверх%
 divBy3(0,0):-!.
-divBy3(X,M):- X1 is X div 10, DivBy3(X1,M1), M2 is X mod 10, (0 is M2 mod 3, M is M2+M1; M is M1),!.
+divBy3(X,M):- X1 is X div 10, divBy3(X1,M1), M2 is X mod 10, (0 is M2 mod 3, M is M2+M1; M is M1),!.
 
 
 
