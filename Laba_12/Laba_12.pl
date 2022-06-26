f6de6da9ff9b58ea42adc00dcbe32c50ca2a6b78
OlegVasil <100850508+OlegVasil@users.noreply.github.com>
2022-06-27 00:55:03 +0300

    Update Laba_12.pl

--- Laba_12/Laba_12.pl
+++ Laba_12/Laba_12.pl
@@ -52,4 +52,86 @@ maxIndexInList([H|T],NowMax,Max,NowIndex,IndexMax,Index):-
 
 countAfterMax([Head|Tail],COUNT):- maxIndexInList([Head|Tail],Max,IndexMax), listleng([Head|Tail],Length), COUNT is (Length-1-IndexMax).
 
-task5:- read(N),readlist(List,N),countAfterMax(List,Count),write(Count),!.
+number15:- read(N),readlist(List,N),countAfterMax(List,Count),write(Count),!.
+
+%____16____%
+minIndexInList([H|T],Min,IndexMin):- minIndexInList([H|T],H,Min,0,IndexMin,0).
+minIndexInList([],NowMin,Min,NowIndex,IndexMin,Index):-Min is NowMin,IndexMin is NowIndex,!.
+minIndexInList([H|T],NowMin,Min,NowIndex,IndexMin,Index):-
+    Index1 is Index+1,
+    (
+        NowMin >= H,
+        minIndexInList(T,H,Min,Index,IndexMin,Index1);
+        minIndexInList(T,NowMin,Min,NowIndex,IndexMin,Index1)
+    ),!.
+
+makeEmptyList([]).
+
+cutList(List,From,To,NewList):-cutList(List,From,To,NewList,0).
+cutList([],From,To,NewList,I):-makeEmptyList(NewList),!.
+cutList([H|T],From,To,NewList,I):-
+    I1 is I+1,
+    (
+        I>=From,
+        (
+            I<To,
+            cutList(T,From,To,NewList1,I1),
+            append([H],NewList1,NewList);
+            makeEmptyList(NewList)
+        );
+        cutList(T,From,To,NewList,I1)
+    ),
+    !.
+
+takeOnIndex(List,Index,Elem):-takeOnIndex(List,Index,Elem,0).
+takeOnIndex([H|T],Index,Elem,I):-
+    I1 is I+1,
+    (
+        I is Index,
+        Elem is H;
+        takeOnIndex(T,Index,Elem,I1)
+    ),
+    !.
+
+swapList(List,NewList):-listleng(List,Length),Length1 is Length-1,swapList(List,NewList,Length1).
+swapList(List,NewList,-1):-makeEmptyList(NewList),!.
+swapList(List,NewList,I):-
+    takeOnIndex(List,I,Elem),
+    I1 is I-1,
+    swapList(List,NewList1,I1),
+    append([Elem],NewList1,NewList),
+    !.
+
+swapBetweenMinAndMax(List,NewList):-
+    listleng(List,Length),
+    maxIndexInList(List,Max,IndexMax),
+    minIndexInList(List,Min,IndexMin),
+    (
+        IndexMax < IndexMin,
+
+        IndexMax1 is IndexMax+1,
+        cutList(List,0,IndexMax1,FirstPart),
+        cutList(List,IndexMax1,IndexMin,SecondPart),
+        cutList(List,IndexMin,Length,ThirdPart);
+        
+        IndexMin1 is IndexMin+1,
+        cutList(List,0,IndexMin1,FirstPart),
+        cutList(List,IndexMin1,IndexMax,SecondPart),
+        cutList(List,IndexMax,Length,ThirdPart)
+    ),
+    swapList(SecondPart,SwapedSecondPart),
+    append(FirstPart,SwapedSecondPart,FSPart),
+    append(FSPart,ThirdPart,NewList).
+
+number16:- read(N),readList(List,N),swapBetweenMinAndMax(List,NewList),writeList(NewList),!.
+
+%____17____%
+countElemBetweenAB([],A,B,Count):-Count is 0,!.
+countElemBetweenAB([H|T],A,B,Count):-
+    (
+        H<B,
+        H>A,
+        countElemBetweenAB(T,A,B,Count1),
+        Count is Count1+1;
+        countElemBetweenAB(T,A,B,Count)
+    ).
